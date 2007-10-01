01b1f1782bc48e706108752b82efd69d758ad5af
martijn.dashorst <martijn.dashorst@f9499c33-a338-0410-a39a-97a3215efc5e>
2007-10-01 09:48:40 +0000

    Updated the examples to reflect the updated chapter 6

--- book-wicket-in-action/src/java/wicket/in/action/chapter06/operations/OperationsPage.java
+++ book-wicket-in-action/src/java/wicket/in/action/chapter06/operations/OperationsPage.java
@@ -1,74 +1,72 @@
 package wicket.in.action.chapter06.operations;
 
+import java.util.Calendar;
+
 import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.repeater.RepeatingView;
 
 /**
- * Created by IntelliJ IDEA.
- * User: dashorst
- * Date: Sep 20, 2007
- * Time: 11:56:53 PM
- * To change this template use File | Settings | File Templates.
+ * This page shows the examples for section 6.4
  */
 public class OperationsPage extends WebPage {
-    public OperationsPage() {
-        WebMarkupContainer example1 = new WebMarkupContainer("example1");
+  public OperationsPage() {
+    createExample6_4_1();
+    createExample6_4_2();
+    createExample6_4_3();
+  }
 
-        example1.add(new Label("label1", "label 1"));
-        example1.add(new Label("label2", "label 2"));
-        example1.add(new Label("label3", "label 3"));
-        example1.add(new Label("label4", "label 4"));
+  private void createExample6_4_1() {
+    // use a webmarkupcontainer to isolate the examples
+    WebMarkupContainer example641 = new WebMarkupContainer("examples-6.4.1");
 
-        add(example1);
+    Label label1 = new Label("label1", "label 1");
+    example641.add(label1.setVisible(false));
+    example641.add(new Label("label2", "label 2").setVisible(!label1
+        .isVisible()));
 
-        example1.get("label3").setVisible(false);
-        example1.get("label4").setVisible(false);
+    example641.add(new Label("label", "Now I'm visible!") {
+      @Override
+      public boolean isVisible() {
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        return day != Calendar.SATURDAY && day != Calendar.SUNDAY;
+      }
+    });
 
-        WebMarkupContainer example2 = new WebMarkupContainer("example2");
+    add(example641);
+  }
 
-        example2.add(new Label("label1", "label 1"));
-        example2.add(new Label("label2", "label 2"));
-        WebMarkupContainer wmc = new WebMarkupContainer("wmc");
-        example2.add(wmc);
-        wmc.add(new Label("label3", "label 3"));
-        wmc.add(new Label("label4", "label 4"));
+  private void createExample6_4_2() {
+    // use a webmarkupcontainer to isolate the examples
+    WebMarkupContainer example642 = new WebMarkupContainer("examples-6.4.2");
+    example642.add(new Label("message1", "Hello, World!") {
+      @Override
+      protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        tag.put("style", "color:red");
+      }
+    });
+    example642.add(new Label("message2", "Some text")
+        .add(new SimpleAttributeModifier("style", "color:red")));
+    Link link = new Link("link") {
+      public void onClick() {
+        System.out.println("Clicked!");
+      }
+    };
+    example642.add(link);
+    link.add(new SimpleAttributeModifier("onclick",
+        "return confirm('Are you sure?');"));
+    add(example642);
+  }
 
-        add(example2);
-
-        wmc.setVisible(false);
-
-        WebMarkupContainer example3 = new WebMarkupContainer("example3");
-        example3.add(new Label("label1", "label 1"));
-        example3.add(new Label("label2", "label 2"));
-        example3.add(new Label("label3", "label 3"));
-        example3.add(new Label("label4", "label 4"));
-
-        add(example3);
-
-        example3.get("label3").setVisible(false);
-        example3.get("label4").setVisible(false);
-
-        WebMarkupContainer example4 = new WebMarkupContainer("example4");
-
-        example4.add(new Label("label", "Some text").add(new SimpleAttributeModifier("style", "color:red")));
-        Link link = new Link("link") {
-            public void onClick() {
-                System.out.println("Clicked!");
-            }
-        };
-        example4.add(link);
-        link.add(new SimpleAttributeModifier("onclick", "return confirm('Are you sure?');"));
-        add(example4);
-
-        RepeatingView rv = new RepeatingView("foo");
-        wmc = new WebMarkupContainer(rv.newChildId());
-        rv.add(wmc);
-        rv.add(new Label("a", "foo"));
-        rv.add(new Label("b", "bar"));
-        add(rv);
-    }
+  private void createExample6_4_3() {
+    // use a webmarkupcontainer to isolate the examples
+    WebMarkupContainer example642 = new WebMarkupContainer("examples-6.4.3");
+    add(example642);
+    
+    example642.add(new Label("message", "Hello, World!").setRenderBodyOnly(true));
+  }
 }
