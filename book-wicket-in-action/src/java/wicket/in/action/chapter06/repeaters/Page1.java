b9d255b8af4135325f6bdf0f9055bec1b9c69e69
martijn.dashorst <martijn.dashorst@f9499c33-a338-0410-a39a-97a3215efc5e>
2007-10-01 09:58:28 +0000

    Some fixes for code

--- book-wicket-in-action/src/java/wicket/in/action/chapter06/repeaters/Page1.java
+++ book-wicket-in-action/src/java/wicket/in/action/chapter06/repeaters/Page1.java
@@ -2,12 +2,5 @@ package wicket.in.action.chapter06.repeaters;
 
 import org.apache.wicket.markup.html.WebPage;
 
-/**
- * Created by IntelliJ IDEA.
- * User: dashorst
- * Date: Sep 19, 2007
- * Time: 4:52:32 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Page1 extends WebPage {
 }
