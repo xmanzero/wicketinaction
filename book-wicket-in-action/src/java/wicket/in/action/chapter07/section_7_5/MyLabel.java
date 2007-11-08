ea879bbf8d518fbed8364b83eba3ed2f8ed63727
martijn.dashorst <martijn.dashorst@f9499c33-a338-0410-a39a-97a3215efc5e>
2007-11-08 22:04:34 +0000

    Chapter 7 examples

---
+++ book-wicket-in-action/src/java/wicket/in/action/chapter07/section_7_5/MyLabel.java
@@ -0,0 +1,10 @@
+package wicket.in.action.chapter07.section_7_5;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+
+public class MyLabel extends WebMarkupContainer {
+
+  public MyLabel(String id) {
+    super(id);
+  }
+}
