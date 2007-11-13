439a1f7b0efabe48bb8eb3be11396f73acaa6e85
martijn.dashorst <martijn.dashorst@f9499c33-a338-0410-a39a-97a3215efc5e>
2007-11-13 11:33:19 +0000

    Removed old chapter 1.4 examples

--- trunk/book-wicket-in-action/src/java/wicket/in/action/chapter01/section_1_4_1/listing01/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package wicket.in.action.chapter01.section_1_4_1.listing01;
-
-import org.apache.wicket.markup.html.WebPage;
-
-public class HelloWorld extends WebPage {
-  public HelloWorld() {
-  }
-}
--- trunk/book-wicket-in-action/src/java/wicket/in/action/chapter01/section_1_4_3/listing01/Page1.java
+++
@@ -1,8 +0,0 @@
-package wicket.in.action.chapter01.section_1_4_3.listing01;
-
-import org.apache.wicket.markup.html.WebPage;
-
-public class Page1 extends WebPage {
-  public Page1() {
-  }
-}
