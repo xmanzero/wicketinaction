b5abaffcc5f290460353a631ef7b339e5e267f78
martijn.dashorst <martijn.dashorst@f9499c33-a338-0410-a39a-97a3215efc5e>
2007-11-13 11:31:14 +0000

    Removed old chapter 3 examples

--- book-wicket-in-action/src/java/wicket/in/action/chapter03/section_3_3_4/listing02/EditContactPage.java
+++
@@ -1,8 +0,0 @@
-package wicket.in.action.chapter03.section_3_3_4.listing02;
-
-import org.apache.wicket.markup.html.WebPage;
-
-public class EditContactPage extends WebPage {
-  public EditContactPage(Contact contact) {
-  }
-}
