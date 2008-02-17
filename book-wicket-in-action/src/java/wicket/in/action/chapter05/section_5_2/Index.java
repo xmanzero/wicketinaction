2bd760acd2748d93469d292ce70e8f1ecd19859b
martijn.dashorst <martijn.dashorst@f9499c33-a338-0410-a39a-97a3215efc5e>
2008-02-17 23:47:07 +0000

    Reimplemented examples from book

--- book-wicket-in-action/src/java/wicket/in/action/chapter05/section_5_2/Index.java
+++ book-wicket-in-action/src/java/wicket/in/action/chapter05/section_5_2/Index.java
@@ -1,15 +1,13 @@
 package wicket.in.action.chapter05.section_5_2;
 
-import org.apache.wicket.markup.html.WebPage;
+import java.io.Serializable;
+
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
 
 import wicket.in.action.AbstractBasePage;
-
-import java.io.Serializable;
 
 /**
  */
@@ -27,25 +25,21 @@ public class Index extends AbstractBasePage {
     }
 
     public Index() {
-        // add label to span tags in the body
-        add(new Label("message", "Hello, World!"));
+      add(new Label("message", "Hello, World!"));
 
-        Person person = new Person();
-        setModel(new CompoundPropertyModel(person));
+      add(new Label("label1", "Hello, World!"));
+      Person person = new Person();
+      add(new Label("label2", new PropertyModel(person, "name")));
+      add(new Label("label3", "Wicket in Action"));
+      add(new Label("name"));
+      add(new Label("label4", new ResourceModel("key", "default value")));
+      
+      add(new Label("message1", "Hello,\nWorld!\nIÕm super!"));
+      add(new MultiLineLabel("message2", "Hello,\nWorld!\nIÕm super!"));
 
-        add(new Label("label1", "Hello, World!"));
-        add(new Label("label2", new PropertyModel(person, "name")));
-        add(new Label("label3", new ResourceModel("key", "default value")));
-        add(new Label("label4", "Wicket in Action"));
-        add(new Label("name"));
-
-        add(new Label("replacer", "All of it gone"));
-
-        add(new Label("label", "Hello,\nWorld!\nIÕm super!"));
-        add(new MultiLineLabel("multiline", "Hello,\nWorld!\nIÕm super!"));
-
-        add(new Label("markup1", "<h1>Hello!</h1>"));
-        add(new Label("markup2", "<h1>Hello!</h1>").setEscapeModelStrings(false));
-        add(new Label("markup3", "<h1 onclick='alert(\"clicked!\");'>Click me</h1>").setEscapeModelStrings(false));
+      add(new Label("message3", "<h1>Hello!</h1>"));
+      add(new Label("message4", "<h1>Hello!</h1>").setEscapeModelStrings(false));
+      add(new Label("message5", "<h1 onclick='alert(\"clicked!\");'>Click me</h1>"));
+      add(new Label("message6", "<h1 onclick='alert(\"clicked!\");'>Click me</h1>").setEscapeModelStrings(false));
     }
 }
